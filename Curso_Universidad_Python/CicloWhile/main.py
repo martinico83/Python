a72d1885feba3148955de15f4dd6696734700786
martinico83 <martin.nicolas.os83@gmail.com>
2022-10-27 08:59:53 -0300

    Universidad Pyton Operador ternario- ciclo while

---
+++ Curso_Universidad_Python/CicloWhile/main.py
@@ -0,0 +1,14 @@
+# condicion = True
+#
+# while condicion:
+#     print('Verdadero')
+# else:
+#     print('Falso')
+
+condicion = 0
+
+while condicion < 3:
+    print(condicion)
+    condicion += 1 #condicion = condicion +1
+else:
+    print('Fin ciclo while')
---
+++ Curso_Universidad_Python/SintaxisIfElseOperadorTernario/main.py
@@ -0,0 +1,12 @@
+# condicion = True
+#
+# if condicion:
+#     print('Verdadero')
+# else:
+#     print
+
+condicion = False
+
+#se recomienda para pocas lineas de codigo. Y es solo para un if else
+
+print('Verdadero') if condicion else print('Falso')
