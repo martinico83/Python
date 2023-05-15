ef04233d978dd71dce9c8ba761df83581362f0ea
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-15 16:44:41 -0300

    tuplas

---
+++ Curso_Universidad_Python/Tuplas/main.py
@@ -0,0 +1,20 @@
+# Definir una Tupla
+
+frutas = ('Naranja', 'Platano', 'Guayaba')
+
+print(len(frutas))
+
+print(frutas)
+
+# Acceder a un elemento
+
+print(frutas[0])
+
+# Navegacion inversa
+
+print(frutas[-1])
+
+#acceder a un rango
+
+print(frutas[0:2]) # sin incluir el ultimo indice
+
