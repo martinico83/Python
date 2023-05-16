6bf0eab298a6cf36d9824021eb5fb8433b35c0fa
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-16 16:52:08 -0300

    Booleanos

---
+++ Booleanos/main.py
@@ -0,0 +1,15 @@
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
+# numero = 5 > 2+3
+# numero = 5 == 2+3
+# numero = 5 >= 2+3
+# numero = 5 != 2+3
+# numero = bool(5 > 2+3 )
+# numero = bool()
+lista = [1,2,3,4]
+# control = 2 in lista
+control = 5 not in lista
+print(type(control))
+print(control)
